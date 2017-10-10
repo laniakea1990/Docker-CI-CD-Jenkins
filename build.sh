36bfa0d50e6a7f480f56673b68bc519f72daadb2
jinranran <jinranran1990910@gmail.com>
2017-10-10 10:07:47 +0800

    Track 1 files into repository.
    - modified build.sh
    
    Auto commit by GitBook Editor

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ git checkout --orphan gh-pages
 git add .
 echo "commit gitbook file"
 git commit -m "Auto publisher"
-git remote add origin https://github.com/laniakea1990/test_book.git 
+git remote add origin https://github.com/laniakea1990/Docker-CI-CD-Jenkins.git 
 echo "push gh-pages branch to origin"
 git push --force -u origin gh-pages > /dev/null 2>&1
